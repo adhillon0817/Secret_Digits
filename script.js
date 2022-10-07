20a2b73e505084c7bbb0998cfe6a5ab60b30193e
adhillon0817 <avleendhillon@gmail.com>
2022-10-06 23:16:57 -0700

    added variables for numbers, uppercase, lowercase, and special characters

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// What sort of data does my application need to work?
+
+// Storing all needed characters in variables.
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "}", "|", "[", "]"]
+
+// What sorts of actions does my code need to complete? 
+
+// Generate a password 
+function generatePassword() {
+
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
